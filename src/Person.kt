4066bff9635f169e508f2e4af2afd1f284ffc5b7
Irma <irblancofdz@gmail.com>
2018-09-05 19:47:28 +0200

    Kotlin default initialization of pvariables in the constructor

--- src/Person.kt
+++ src/Person.kt
@@ -1,20 +1,14 @@
-//Person(name: String) es el constructor primario
-class Person(name: String) {
+//Person(name: String) is the primary constructor
+class Person(var name: String) {
 
-    //Atributos de clase
-    var name: String
-
-    //constructor secundario con nombre y apellido
+    //Secondary constructor
     constructor(name: String, lastname: String) : this(name)
 
-    //Init se ejecuta siempre que se ejecuta el constrctor primario
     init {
-        //this.name es la variable de clase, atributo de clase y name, lo que asignamos, es el nombre que nos pasan al inicializar la instancia del constructor
-        this.name = name
-        print("Born")
+        print("Born!\n")
     }
 
     fun greeting() {
-        print("Hello world, my name is  ${name}")
+        print("Hello world, my name is $name \n")
     }
 }
--- src/app.kt
+++ src/app.kt
@@ -1,4 +1,5 @@
 fun main(args: Array<String>) {
     val jon: Person = Person("Jon", "Snow")
-
+    jon.greeting()
+    print(jon.name)
 }
